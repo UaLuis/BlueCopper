859a8af9bad37b7cb717d625f4dc282d719bb1ef
UaLuis <ua.luis01@gmail.com>
2026-03-04 12:44:30 +0200

    first commit

---
+++ Python_Bp/all_functions/base_function.py
@@ -0,0 +1,14 @@
+def create_print(DATA):
+    if type(DATA) is int:
+        print(DATA)
+    elif '"' in DATA:
+        print(DATA[1:-1])
+    else:
+        print(DATA)
+
+def create_if(condition, block):
+    pass
+    
+def create_create(name, args, block): {
+    
+}
