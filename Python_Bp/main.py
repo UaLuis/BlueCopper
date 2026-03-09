0985daeb06bf51951c2dd55af3c95d423eb2bad6
UaLuis <ua.luis01@gmail.com>
2026-03-09 16:52:24 +0200

    Fix bugs

--- Python_Bp/main.py
+++ Python_Bp/main.py
@@ -1,10 +1,11 @@
 import parser as pr
 import sys
 
-#script_name = sys.argv(1)
+# script_name = sys.argv(1)
 
 code = []
 
 code = pr.read_file("/Users/wolfi/BlueCopper/tests/test0.bp")
-print(code)
-pr.check_base_structure(code)
+
+index_data, index_data_end, index_start, index_start_end = pr.check_base_structure(code)
+pr.parser(code, index_start, index_data, index_data_end, index_start_end)
--- Python_Bp/parser.py
+++ Python_Bp/parser.py
@@ -1,22 +1,23 @@
 from all_functions import base_function as bsf
-from all_functions import math_function as mhf
 import shlex
 
-vars = {}
+variables = {}
 
 def create_var(name, value, type):
     if type == 'int':
-        vars[name] = int(value)
+        variables[name] = int(value)
     elif type == 'string': 
-        vars[name] == str(value)
+        variables[name] = str(value)
     elif type == 'float':
-        vars[name] == float(value)
+        variables[name] = float(value)
     elif type == 'bool':
-        vars[name] == bool(value)
+        variables[name] = bool(value)
     else:
         print(f'Помилка! Не існує типу {type}!')
+        
+def resolve_args(args):
+    return [variables.get(a, a) for a in args]
     
-
 def read_file(script_name):
     code = []
     
@@ -33,13 +34,19 @@ def read_file(script_name):
     
 def check_base_structure(code):
     index_data = None
-    index_data_end = None 
-
+    index_data_end = None
+    index_start = None
+    index_start_end = None
+    
     for i, line in enumerate(code):
         if line[0] == '.data:' and index_data is None:
             index_data = i
         if line[0] == '.data_end' and index_data_end is None:
             index_data_end = i
+        if line[0] == '.start:' and index_start is None:
+            index_start = i
+        if line[0] == '.start_end' and index_start_end is None:
+            index_start_end = i
 
     if code[0][0] == '.start:' and code[index_data-1][0] == '.start_end':
         if code[index_data][0] == '.data:' and code[index_data_end][0] == '.data_end':
@@ -48,9 +55,6 @@ def check_base_structure(code):
             print("ERROR in .data section!")
     else:
         print("ERROR in .start section!")
-        
-    index_start = code[0][0]
-    index_start_end = code[index_data-1][0]
     
     return index_data, index_data_end, index_start, index_start_end
     
@@ -59,7 +63,21 @@ def check_structure(code, type_structure):
     
 def parser(code, index_start, index_data, index_data_end, index_start_end):
     #Читання блоку .data та створення змінних
-    for i in range(1, index_start_end):
+    for i in range(index_data + 1, index_data_end):
         name, value, type = code[i][1], code[i][2], code[i][0]
-        create_var(
+        create_var(name, value, type)
+   
+   #Читання блоку .start та запуск коду 
+    for i in range(index_start + 1, index_start_end):
+        command, args = code[i][0], code[i][1:]
+        args = resolve_args(args)
+        fun_run = getattr(bsf, f"{command}_", None)
+        
+        if fun_run:
+            fun_run(*args)
+        else:
+            print(f"ERROR! {command} невідома команда!")
+
+
+        
     
